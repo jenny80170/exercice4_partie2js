64e42163b4c66f1339546b96df20d102f6eec29e
jenny80170 <jennifer.malleret@outlook.fr>
2019-10-25 14:32:16 +0200

    Effacer les données des champs du formulaire

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Fonction qui permet d'activé le onclick dans le html
+function resetForm() {
+  //value = '' permet de remettre le champ à vide
+  document.getElementById('lastname').value = '';
+  document.getElementById('firstname').value = '';
+  document.getElementById('city').value = '';
+}
